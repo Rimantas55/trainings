afadfd632d1996204f4948e5fe7db3713f3f78e1
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2020-12-22 00:41:04 +0200

    practice with MVC added folder structure

---
+++ mvcDary/app/require.php
@@ -0,0 +1,6 @@
+<?php
+  // require libraries from folder libraries
+  // connects all libraries files with public/index.php
+  require_once 'libraries/Core.php';
+  require_once 'libraries/Controller.php';
+  require_once 'libraries/Database.php';
