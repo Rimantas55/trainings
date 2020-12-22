042b065c5bb261e3016e83c5c2093b65e6f69a06
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2020-12-22 21:45:42 +0200

    find right method from url, tested

--- mvcDary/app/controllers/Pages.php
+++ mvcDary/app/controllers/Pages.php
@@ -4,8 +4,17 @@ class Pages {
 
   public function __construct()
   {
-    echo 'hello  from pages - loaded';
+    echo 'hello  from Pages controller construct method - loaded' . "<br>";
     
   }
 
+  public function index() 
+  {
+    echo "Home Page - render from Pages class from index method" . "<br>";
+  }
+
+  public function about() 
+  {
+    echo "About Page - render from Pages class from about method" . "<br>";
+  }
 }
--- mvcDary/app/libraries/Core.php
+++ mvcDary/app/libraries/Core.php
@@ -4,7 +4,7 @@
     // if there are no any controllers in out controller folder this page will be automaticaly loaded
     protected $currentController = 'Pages'; 
     // inside Pages controller it will load index method
-    protected $currentMetchod = 'index'; 
+    protected $currentMethod = 'index'; 
     // $currentController and $currenMethod chagnes with URL changes and we need create method to fetch that URL
     // ect localhost/projectname/shop/glasses/men/ - shop/glasses/men/ should be putted inside params, 
     // so, index0 - shop, index1 - glasses, index2 - men
@@ -21,16 +21,32 @@
 
         // Look in 'controllers' folder for first value, ucwords() will capitalize first letter, because classes will be named with first  capital letter 
         // Look for the right file
+        // check the first part of url - index0 which will stand for Class name in controller folder
+        // localhost/projectname/index[0]CLassName/index1 - glasses - method/men/
         if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
           // will set a new controller
           $this->currentController = ucwords($url[0]);
           unset($url[0]);
         }
 
+        
         // require the controller 
         require_once '../app/controllers/' . $this->currentController . '.php';
         $this->currentController = new $this->currentController; // initiate class
+        
+        // check the second part of url - index1 which is stands for method
+        // localhost/projectname/index[0]CLassName/index1 - glasses - method/men/
+        if(isset($url[1])){
+          if(method_exists($this->currentController, $url[1])) {
+            $this->currentMethod = $url[1];
+            unset($url[1]);
+          }
+        }
+        // get parameters
+        $this->params = $url ? array_values($url) : [];
 
+        // call a call back with array of parameters
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
     public function getUrl() 
